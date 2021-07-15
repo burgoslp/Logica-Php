01151d5579e3fc48a66bddad65455941aa46af60
leopoldo <pinedo.burgoslp3@gmail.com>
2021-07-15 05:59:05 -0400

    agregando ejercicio 52

---
+++ practica52/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio  52 de php Lógica de programación</title>
+</head>
+<body>
+    <pre>
+        Crear un crud con las cuatros funcionalidades (Crear, listar, actualizar, borrar) 
+        la base de datos se llama php, la tabla sera users(id,sexo_id,estadoCiv_id,nombre,apellido,correo)
+        tiene una relacion con otras dos tablas sexo(id,descripcion), estadoCiv(id,descripcion)
+        
+
+
+
+    </pre>
+</body>
+</html>
